9c22d56062b2f599ad6c6f73010213f7ccd2fa6c
jsyang <home9999@naver.com>
2020-10-12 11:33:48 +0900

    db ip modified

--- src/main/java/com/controller/DietApiController.java
+++ src/main/java/com/controller/DietApiController.java
@@ -7,10 +7,7 @@ import com.service.DietService;
 import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -23,6 +20,11 @@ public class DietApiController {
 
     @Autowired
     private DietService dietService;
+
+    @GetMapping("/")
+    public String main() {
+        return "api.wellbeeing";
+    }
 
     @ApiOperation(value="한 주 식단리스트 조회", notes = "")
     @PostMapping("/api/weekDietList")
