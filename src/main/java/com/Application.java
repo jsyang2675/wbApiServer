b0351cc17f0ab117af926f8c91ca115cffa1e001
jsyang <home9999@naver.com>
2020-10-17 22:13:12 +0900

    errorpage filter 동작 안하도록

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -9,6 +9,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    public Application() {
+        super();
+        setRegisterErrorPageFilter(false);
+    }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
